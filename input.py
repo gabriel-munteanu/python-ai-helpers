3f146fea6978088f973ce5d2c8c3a641bc231e53
Gabriel Munteanu <wyrdagarjzla@yahoo.com>
2018-06-11 23:27:25 +0300

    Initial plans

---
+++ input.py
@@ -0,0 +1,5 @@
+
+#doar numpy array
+#de/normalizare 0 1
+#de/normalizare -1 1
+#split traint, dev, test
---
+++ plot.py
@@ -0,0 +1,5 @@
+#plot 2 sau mai multe linii pe acelasi grafic
+#scatter plot 2d
+#scatter plot 3d
+#plot learning/loss curves
+#plot prediction vs actual
