ed82864142b7a6954e86c013417d71dcf28f6f09
Gabriel Munteanu <wyrdagarjzla@yahoo.com>
2018-06-17 22:05:57 +0300

    Finish implementing input functions

--- input.py
+++ input.py
@@ -1,15 +1,10 @@
 import numpy as np
 
-# doar numpy array
 
-
-"""
-Normalize data between 0 and 1
-
-Warning! New input for networks must have the same min and max as 
-the training data, for correct results
-"""
 def normalize_0_1(data):
+    # Normalize data between 0 and 1
+    # Warning! New input for networks must have the same min and max as
+    # the traininging data, for correct results
     offset_and_scale = []
     data = data.astype(float)
     _, cols = data.shape
@@ -20,13 +15,10 @@ def normalize_0_1(data):
     return data, offset_and_scale
 
 
-"""
-Normalize data between -1 and 1
-
-Warning! New input for networks must have the same min and max as 
-the training data, for correct results
-"""
 def normalize_m1_1(data):
+    # Normalize data between -1 and 1
+    # Warning! New input for networks must have the same min and max as
+    # the traininging data, for correct results
     offset_and_scale = []
     data = data.astype(float)
     _, cols = data.shape
@@ -40,26 +32,38 @@ def normalize_m1_1(data):
     return data, offset_and_scale
 
 
-"""
-Denormalize data
-"""
 def denormalize(data, offset_and_scale):
+    # Denormalize data
     _, cols = data.shape
     for col in range(cols):
         data[:, col] = data[:, col] * \
             offset_and_scale[col][1] + offset_and_scale[col][0]
     return data
 
-# split traint, dev, test
+
+def split_data(data, training_pct=0.6, dev_pct=0.2):
+    # split training, dev, test
+    test_pct = 1 - training_pct - dev_pct
+    if test_pct < 0:
+        raise Exception('training percent and dev percent exced 100%')
+    length = len(data)
+    training_length = int(length * training_pct)
+    training = data[0:training_length]
+    dev_length = int(length * dev_pct)
+    dev = data[training_length:training_length + dev_length]
+    test = data[training_length + dev_length:]
+    return training, dev, test
 
 
-def split_data(data):
-    # todo
-    return data
+# test_data = np.array([[1, 2], [2, 3], [3, 4]])
+# print(test_data)
+# print(test_data.shape)
+# res, mw = normalize_m1_1(test_data)
+# print(denormalize(res, mw))
 
 
-test_data = np.array([[1, 2], [2, 3], [3, 4]])
-print(test_data)
-print(test_data.shape)
-res, mw = normalize_m1_1(test_data)
-print(denormalize(res, mw))
+# test_data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+# training, dev, test = split_data(test_data)
+# print(training)
+# print(dev)
+# print(test)
